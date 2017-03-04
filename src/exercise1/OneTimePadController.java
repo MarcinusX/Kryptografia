df027ec9371d94dd5a7c3f62b7962c78a291dd0e
Marcinus <szalek95@gmail.com>
2017-03-04 15:45:53 +0100

    init

---
+++ src/exercise1/OneTimePadController.java
@@ -0,0 +1,7 @@
+package exercise1;
+
+/**
+ * Created by szale_000 on 2017-03-04.
+ */
+public class OneTimePadController {
+}
---
+++ src/exercise2/RabinAlgorithmController.java
@@ -0,0 +1,7 @@
+package exercise2;
+
+/**
+ * Created by szale_000 on 2017-03-04.
+ */
+public class RabinAlgorithmController {
+}
