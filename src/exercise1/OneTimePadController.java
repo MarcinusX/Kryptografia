d4168ae49eff5f470aaf138257954e05685212d2
krystian <krystian@BossPC>
2017-03-05 12:06:52 +0100

    Refactoring kodu. Wyświetlanie wprowadzonego tekstu, klucza i zaszyfrowanego tekstu w postaci Hex

--- src/exercise1/OneTimePadController.java
+++ src/exercise1/OneTimePadController.java
@@ -6,51 +6,75 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
 import java.security.SecureRandom;
+import java.util.Arrays;
 
 /**
  * Created by szale_000 on 2017-03-04.
  */
 public class OneTimePadController {
+
+    private static final Charset CHARSET = Charset.forName("UTF-8");
+
     public TextField textFieldInput;
     public Button buttonChooseFile;
     public Label labelFilePath;
     public Button buttonEncrypt;
     public Label labelByteText;
+    public Label labelByteTextHex;
     public Label labelKey;
     public Label labelCypherText;
     public Button buttonDecrypt;
     public Label labelDecodedText;
 
+    private String textToEncrypt;
+    
+    private byte[] bytesFromOrgText;
     private byte[] key;
     private byte[] cyphered;
 
-    //TODO: Wczytywanie z pliku
-
-    @FXML
-    public void handleEncryptClick() {
-        try {
-            byte[] plainText = textFieldInput.getText().getBytes("utf-8");
-            key = generateKey(plainText.length);
-            cyphered = encryptText(plainText, key);
-
-            //TODO: Tutaj jest pokazany problem:
-            //jak mamy bajtowy tekst zaszyfrowany to on ma określoną ilość bajtów
-            //jeśli te bajty zapisujemy do stringa a potem znowu do tablicy bajtów to ta wartość się zmienia
-            //Innymi słowy: póki co opercja byte[]->string->byte[] jest nieodwracalna
-            //Dlatego zapisujemy tekst zaszyfrowany w polu prywatnym,, a nie w labelu widoku.
-            String x = "" + cyphered;
-            byte[] y = x.getBytes("UTF-8");
-
-            //Poza tym te tablice bajtów wyświetlają się tak jak się wyświetlają, jak coś dasz radę z tym zrboić to super
-            labelByteText.setText(new String(plainText, "UTF-8"));
-            labelKey.setText(new String(key, "UTF-8"));
-            labelCypherText.setText(new String(cyphered, "UTF-8"));
-        } catch (Exception e) {
-
+    public String byteToHex(byte b) {
+        int i = b & 0xFF;
+        String tmpHex = Integer.toHexString(i);
+        String returnHex = "";
+        if (tmpHex.length() < 2){
+            returnHex = "0" + tmpHex;
+        } else {
+            returnHex = tmpHex;
         }
+       
+        return returnHex;
     }
 
+    public String printBytes(byte[] array) {
+        String hexNumber = "";
+        for (int k = 0; k < array.length; k++) {
+            hexNumber += "0x" + byteToHex(array[k]) + " ";
+        }
+        return hexNumber;
+    }
+
+    private void printLabels(){
+        labelByteText.setText(textToEncrypt);
+        labelByteTextHex.setText(printBytes(bytesFromOrgText));
+        labelKey.setText(printBytes(key));
+        labelCypherText.setText(printBytes(cyphered));
+    }
+    
+    @FXML
+    public void handleEncryptClick() {
+        textToEncrypt = textFieldInput.getText();
+
+        bytesFromOrgText = textToEncrypt.getBytes(CHARSET);
+        key = generateKey(bytesFromOrgText.length);
+        cyphered = encryptText(bytesFromOrgText, key);
+        
+        printLabels();
+    }
+    
+    
     private byte[] encryptText(byte[] plainText, byte[] key) {
         byte[] encryptedText = new byte[plainText.length];
         for (int i = 0; i < plainText.length; i++) {
@@ -61,13 +85,9 @@ public class OneTimePadController {
 
     @FXML
     public void handleDecryptClick() {
-        try {
-            byte[] decrypted = encryptText(cyphered, key);
+        byte[] decrypted = encryptText(cyphered, key);
 
-            labelDecodedText.setText(new String(decrypted, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        labelDecodedText.setText(new String(decrypted, CHARSET));
     }
 
     private byte[] generateKey(int length) {
