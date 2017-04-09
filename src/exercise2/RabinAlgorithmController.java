ac9ea716e1377c4b2655e539d4e65398ae96eab1
Monika <monika.stobieniecka1@wp.pl>
2017-04-09 18:48:27 +0200

    Dodanie implementacji algorytmu Rabina -poprawki

--- src/exercise2/RabinAlgorithmController.java
+++ src/exercise2/RabinAlgorithmController.java
@@ -28,14 +28,37 @@ public class RabinAlgorithmController {
                 "C:\\Users\\Monia\\Documents\\_STUDIA\\studia - 6 semestr\\KRYPTOGRAFIA\\Kryptografia\\src\\exercise2\\rabinAlgorithm\\rabinWrapper.py",
                 textToEncrypt,
         };
-        try {
-            Process p = Runtime.getRuntime().exec(cmd);
-            System.out.print("bbb");
+        String s = null;
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            System.out.print("aaaa");
-            System.out.println(in.readLine());
-        } catch (Exception e) {
+        try {
+
+            // run the Unix "ps -ef" command
+            // using the Runtime exec method:
+            Process p = Runtime.getRuntime().exec(cmd);
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            System.exit(-1);
         }
     }
 
--- src/exercise2/rabinAlgorithm/rabinWrapper.py
+++ src/exercise2/rabinAlgorithm/rabinWrapper.py
@@ -10,9 +10,6 @@ def main(message):
     print (millerRabinPrimalityTest(privateKey_p, 10))
     privateKey_q = privateKey['q']
 
-    decrypt(privateKey_p, privateKey_q, encrypt(message, privateKey_p * privateKey_q))
-
-    print(int(5))
-
+    print(decrypt(privateKey_p, privateKey_q, encrypt(message, privateKey_p * privateKey_q)))
 
 main(message=5)
